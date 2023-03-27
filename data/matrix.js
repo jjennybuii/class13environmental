2a45bd3ad506547ac39eb74c0b08bdc2605e9969
jjennbui <jbui22@my.bcit.ca>
2023-03-27 14:42:41 -0700

    adding files and folders

---
+++ data/matrix.js
@@ -0,0 +1,32 @@
+export const matrix = {
+    courses: [
+        {
+            term: 1,
+            code: "COMM 1118",
+            title: "Communication for Digital Design and Development 1",
+            credits: 3.0,
+            description: "rahrahrahrah"
+        },
+        {
+            term: 2,
+            code: "COMM 2218",
+            title: "Communication for Digital Design and Development 2",
+            credits: 4.0,
+            description: "description 2"
+        },
+        {
+            term: 3,
+            code: "COMP 3370",
+            title: "Frontend Web Development 2",
+            credits: 3.0,
+            description: "description 3"
+        },
+        {
+            term: 4,
+            code: "BLAW 4418",
+            title: "Internet and IT Law",
+            credits: 3.0,
+            description: "description 4"
+        }
+    ]
+}
